65595829ea2dd6e1ddcb9b96ca1a251aef071e05
chongt <chongt@users.noreply.github.com>
2015-09-01 23:01:05 -0400

    Create helloDgramClient.py

---
+++ HW/2-Socket-Exercise/helloDgramClient.py
@@ -0,0 +1,14 @@
+# helloDgramClient.py
+# Purpose: Client for sockets from corresponding server.
+
+from socket import *
+serverName = '172.17.149.120'
+serverPort = 21801
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = raw_input('Input lowercase sentence: ')
+clientSocket.sendto(message,(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print modifiedMessage
+clientSocket.close()
+
+
