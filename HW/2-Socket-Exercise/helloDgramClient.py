c9cb57f91335602afeb5705488f757a215717c9a
chongt <chongt@users.noreply.github.com>
2015-09-01 23:08:04 -0400

    Update helloDgramClient.py

--- HW/2-Socket-Exercise/helloDgramClient.py
+++ HW/2-Socket-Exercise/helloDgramClient.py
@@ -5,7 +5,7 @@ from socket import *
 serverName = '172.17.149.120'
 serverPort = 21801
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = raw_input('Input lowercase sentence: ')
+message = raw_input('Input lowercase sentence to be converted into UPPERCASE: ')
 clientSocket.sendto(message,(serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print modifiedMessage
