fbf3b68456dfe55c7e2ea755ce7dd231271af371
chongt <chongt@users.noreply.github.com>
2015-09-01 23:00:21 -0400

    Create helloDgramServer.py

---
+++ HW/2-Socket-Exercise/helloDgramServer.py
@@ -0,0 +1,12 @@
+# helloDgramServer.py
+# Purpose: Create datagram socket for corresponding client.
+
+from socket import *
+serverPort = 21801
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
+print "The server is ready to receive"
+while 1:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    modifiedMessage = message.upper()
+    serverSocket.sendto(modifiedMessage, clientAddress)
