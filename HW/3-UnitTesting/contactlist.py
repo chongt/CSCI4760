65aa91032087ff857b6965170cd5c4ef4b0466be
chongt <chongt@users.noreply.github.com>
2015-10-11 21:24:18 -0400

    Create contactlist.py

---
+++ HW/3-UnitTesting/contactlist.py
@@ -0,0 +1,31 @@
+# class for contact list
+# just stub code destined for failure against testing
+
+class contactlist:
+    def _init_(self):
+        # does nothing
+        pass
+    
+    # Adds an entry into the lists of contacts
+    def addEntry(self):
+        return 0
+        
+    # Returns the email address corresponding to someone.
+    def email(self,someone):
+        return "Not implemented"
+        
+    # Return phone number corresponding to someone.
+    def phoneNum(self,someone):
+        return "Not implemented"
+    
+    # Changes someone's phone number.
+    def changePhone(self,someone):
+        return 0
+
+    # Changes someone's email address
+    def changeEmail(self):
+        return 0
+    
+    # Remove Entry
+    def removeEntry(self):
+        return 0
