cd8e30ebd8563fb88aa713c7c902f7c80e00ac73
chongt <chongt@users.noreply.github.com>
2015-10-11 22:46:19 -0400

    added comments

--- HW/3-UnitTesting/changemaker.py
+++ HW/3-UnitTesting/changemaker.py
@@ -20,29 +20,38 @@ class ChangeMaker:
         
         # Implemented by Tiffany Chong
         
-        self.amount = amount
+        self.amount = amount # get given amount of change to be processed
     
         q = 0 # number of quarters
         d = 0 # number of dimes
         n = 0 # number of nickels
         p = 0 # number of pennies
         
+        # loop to obtain number of quarters by substracting 25 from given amount
         while amount >= 25:
             amount = amount - 25
             q = q + 1
+        # after loop, amount should be less than 25
         
+        # loop to obtain number of dimes by substracting 10 from current amount
         while amount >= 10:
             amount = amount - 10
             d = d + 1
+        # after loop, amount should be less than 10 
         
+        # loop to obtain number of nickels by substracting 5 from current amount
         while amount >= 5:
             amount = amount - 5
             n = n + 1
+        # after loop, amount should be less than 5
         
+        # loop to obtain number of pennies by substracting 1 from current amount
         while amount > 0:
             amount = amount - 1
             p = p + 1
-            
+        # after loop, amount should be 0 
+        
+        #return numbers of coins in an array
         return [q,d,n,p]
 
     def changelist(self, purchase_amount, cash_tendered):
@@ -53,7 +62,8 @@ class ChangeMaker:
         `'''
         
         # Implemented by Tiffany Chong
-        
-        amount = cash_tendered - purchase_amount
-        
+    
+        amount = cash_tendered - purchase_amount # obtain amount of change to be processed
+
+        # call the coinlist method to process amount to return array of coins
         return ChangeMaker.coinlist(self,amount)
